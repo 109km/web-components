fce8eb81e0b4603e8e85b5a829ffdb6b9769e78a
xinheng.shao <shaoxinheng@gmail.com>
2018-11-30 10:56:11 +0800

    up

---
+++ elements/ox-input-wangning/ox-input-wangning.js
@@ -0,0 +1,47 @@
+import {
+  html,
+  PolymerElement
+} from '@polymer/polymer/polymer-element.js';
+
+/**
+ * @name ox-button
+ */
+class OXInput extends PolymerElement {
+  constructor() {
+    super();
+    //this.addEventListener('click', this.onClick);
+  }
+  static get template() {
+    return html `
+      <style>
+        @import '../elements/ox-input-wangning/ox-input-wangning.css';
+      </style>
+      <input type="{{type}}" placeholder="{{placeholder}}" disabled="{{disabled}}"/>
+    `;
+  }
+  static get properties() {
+    return {
+      placeholder: {
+        type: String,
+        value:'11',
+      },
+      type: {
+        type: String,
+        value:"",
+      },
+      disabled: {
+        type: Boolean,
+        value:false,
+      }
+    };
+  }
+  ready() {
+    super.ready();
+    //this.className = `ox-popup ox-popup-${this.data-derection}`;
+  }
+  // onClick(e){
+  //   if (this.hasAttribute('disabled')) return;
+  // }
+}
+
+window.customElements.define('ox-input', OXInput);
