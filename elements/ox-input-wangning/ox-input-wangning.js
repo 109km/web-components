ff60390116b511961973fc820d723dfbe15ede3a
wangning <1051318769@qq.com>
2018-12-13 19:21:46 +0800

    input修改

--- elements/ox-input-wangning/ox-input-wangning.js
+++ elements/ox-input-wangning/ox-input-wangning.js
@@ -14,9 +14,26 @@ class OXInput extends PolymerElement {
   static get template() {
     return html `
       <style>
-        @import '../elements/ox-input-wangning/ox-input-wangning.css';
-      </style>
-      <input type="{{type}}" placeholder="{{placeholder}}" disabled="{{disabled}}"/>
+        :host(.ox-input) .ox-inner-input {
+          display: inline-block;
+          cursor: pointer;
+          padding: 8px 20px;
+          border-radius: 4px;
+          border:1px  var(--color-black) solid;
+        }
+        :host(.ox-input) .ox-input-error-word{
+          display:none;
+          color: var(--theme-color-error);
+        }
+        :host([pressed]) .ox-input-error-word{
+          display:block;
+        }
+        :host(.ox-input) .ox-inner-input-error{
+          border:1px var(--theme-color-error) solid;
+        }
+      </style> 
+      <input class="ox-inner-input" type="{{type}}" placeholder="{{placeholder}}" disabled="{{disabled}}"/>
+      <div class="ox-input-error-word">输入有误</div>
     `;
   }
   static get properties() {
@@ -27,7 +44,7 @@ class OXInput extends PolymerElement {
       },
       type: {
         type: String,
-        value:"",
+        value:'primary',
       },
       disabled: {
         type: Boolean,
@@ -37,7 +54,7 @@ class OXInput extends PolymerElement {
   }
   ready() {
     super.ready();
-    //this.className = `ox-popup ox-popup-${this.data-derection}`;
+    this.className = `ox-input ox-input-${this.type}`;
   }
   // onClick(e){
   //   if (this.hasAttribute('disabled')) return;
