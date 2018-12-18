86e38edd120648c6929a68d798da89b87f09403c
wangdi <2350311176@qq.com>
2018-12-18 18:54:04 +0800

    up

--- elements/ox-radio/ox-radio.js
+++ elements/ox-radio/ox-radio.js
@@ -10,12 +10,53 @@ class OXRadio extends PolymerElement {
   constructor() {
     super();
     this.addEventListener('click', this.onClick);
-    console.log(this.tagName);
   }
   static get template() {
     return html `
     <style>
-      @import '../elements/ox-radio/ox-radio.css';
+    :host{
+      cursor: default;
+    }
+    .ox-radio-outer {
+      display: inline-block;
+      width: 20px;
+      height: 20px;
+      border-radius: 50%;
+      text-align: center;
+      border: 1px solid var(--theme-color-default-border);
+    }
+    :host(.ox-radio-actived) .ox-radio-outer {
+      background-color: var(--theme-color-primary);
+      border-color: var(--theme-color-primary);
+    }
+    .ox-radio-label{
+      display: inline-block;
+    }
+    .ox-radio-inner {
+      display: inline-block;
+      width: 10px;
+      height: 10px;
+      border-radius: 50%;
+      background-color: var(--color-white);
+      margin-top: 5px;
+    }
+    :host([disabled]){
+      cursor: not-allowed;
+
+    }
+    :host([disabled]) .ox-radio-outer{
+      background-color: var(--theme-color-radio-disabled);
+    }
+    :host([disabled]) .ox-radio-inner {
+      visibility: hidden;
+    }
+    :host(.ox-radio-actived[disabled]) .ox-radio-outer{
+      background-color: var(--theme-color-radio-active-disabled);
+      border-color: var(--theme-color-radio-active-disabled);
+    }
+    :host(.ox-radio-actived[disabled]) .ox-radio-inner {
+      visibility: visible;
+    }
     </style>
     <div class="ox-radio-outer">
       <div class="ox-radio-inner">
@@ -44,7 +85,7 @@ class OXRadio extends PolymerElement {
         type: String,
         value: ''
       }
-    };
+    }; 
   }
   onClick(e) {
     if (this.hasAttribute('disabled')) return;
@@ -53,10 +94,9 @@ class OXRadio extends PolymerElement {
       radio.className = '';
       radio.removeAttribute('actived');
     });
-
     this.checked = true;
     this.className = "ox-radio-actived";
-    console.log('此选项值为' + this.value);
+    console.log("选值为",this.value);
   }
   getGroupRadios() {
     return document.querySelectorAll(`[group=${this.group}]`);
--- elements/ox-switch-wangdi/ox-switch-wangdi.js
+++ elements/ox-switch-wangdi/ox-switch-wangdi.js
@@ -15,7 +15,37 @@ class OXSwitch extends PolymerElement {
     static get template() {
         return html `
         <style>
-            @import '../elements/ox-switch-wangdi/ox-switch-wangdi.css';
+        .ox-switch-outer {
+            cursor: pointer;
+            display: inline-block;
+            width: 50px;
+            height: 25px;
+            border-radius: 15px;
+            position: relative; 
+            transition-duration: 0.8s;
+            background-color: var(--theme-bg-color-default); 
+            box-shadow: 0 0 5px rgb(221, 218, 218);
+          }
+          .ox-switch-inner {
+            width: 21px;
+            height: 21px;
+            border-radius: 50%;
+            position: absolute;
+            left: 4px;
+            top: 6%;
+            transition-duration: 0.5s;
+            transform:translateX(-10%);
+            background-color: var(--color-white);
+          }
+          :host(.ox-switch-actived) .ox-switch-outer {
+            transition-duration: 0.5s;
+            background-color: var(--theme-bg-color-primary);
+            box-shadow: 0 0 5px rgb(238, 176, 61);
+          }
+          :host(.ox-switch-actived) .ox-switch-inner {
+            transition-duration: 0.5s;
+            left: 30px;
+          }
         </style>
             <div class="ox-switch-outer">
                 <div class="ox-switch-inner"></div>
@@ -43,7 +73,7 @@ class OXSwitch extends PolymerElement {
     onClick(e) {
         if (this.hasAttribute('disabled')) return;
         if (this.hasAttribute('checked')) {
-            console.log(this.hasAttribute('checked'));
+            console.log('关闭:',this.hasAttribute('checked'));
             this.removeAttribute('checked');
             this.className = '';
             this.checked = false;
@@ -51,12 +81,12 @@ class OXSwitch extends PolymerElement {
             this.checked = true;
             this.setAttribute('checked', true);
             this.className = 'ox-switch-actived';
+            console.log('打开:',this.hasAttribute('checked'));
           }
     }
        setDisabled() {
         this.setAttribute('disabled');
       }
-    
       removeDisabled() {
         this.removeAttribute('disabled');
       }   
