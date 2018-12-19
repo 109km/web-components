3419e2c236e49733a91554a742dc3f75204151e4
Admin-PC\Admin <wangfanglan@sunlands.com>
2018-12-19 18:33:57 +0800

    no message

--- elements/ox-bubble-wangfanglan/ox-bubble.js
+++ elements/ox-bubble-wangfanglan/ox-bubble.js
@@ -9,6 +9,11 @@ class OXBubble extends PolymerElement {
   constructor() {
     super();
     //hasTitle();
+<<<<<<< HEAD
+=======
+    this.addEventListener('mouseenter', this.onMouseenter);
+    this.addEventListener('mouseleave', this.onMouseleave);
+>>>>>>> develop
   }
   static get template() {
     return html `
@@ -16,12 +21,23 @@ class OXBubble extends PolymerElement {
     @import '../elements/ox-bubble-wangfanglan/ox-bubble.css';
     </style>
         <div class="ox-bubble">
+<<<<<<< HEAD
             <slot></slot>
+=======
+          {{director}}
+          <div class$="ox-bubble-box [[mask]] ox-bubble-box-[[type]]">
+            <div class="title">{{titleContent}}</div>
+            <div class="content">{{content}}</div>
+            <div class$="triangle triangle-[[type]]">
+            </div>
+          </div>
+>>>>>>> develop
         </div>
     `;
   }
   static get properties() {
     return {
+<<<<<<< HEAD
       oxClass:{
         type:String,
         value: '',
@@ -32,6 +48,65 @@ class OXBubble extends PolymerElement {
   ready() {
     super.ready();
     this.className = `ox-bubble ${this.oxClass}`;
+=======
+      director: {
+        type: String,
+        value: ''
+      },
+      content:{
+        type: String,
+        value: ''
+      },
+      mask: {
+        type:String,
+        value:'hide'
+      },
+      type: {
+        type:String,
+        value:'left'
+      },
+      titleContent: {
+        type: String,
+        value: ''
+      }
+    };
+  }
+  onMouseenter(e) {
+    console.log("鼠标进入");
+    this.mask = "show";
+    var title = this.titleContent;
+    console.log(title);
+    if(title !== ""){
+      //var a = document.querySelector(".title");
+      console.log(this.shadowRoot.children[1].children[0].children[2].className);
+      this.shadowRoot.children[1].children[0].className += " ox-bubble-box-ok";
+      this.shadowRoot.children[1].children[0].children[2].className += ' triangle-' + this.type + '-ok';
+      //console.log(e.target.children);
+      //e.target.children.setAttribute('class','ox-bubble-wangfanglan-box-ok');
+    }else{
+      console.log(0)
+    }
+  }
+  onMouseleave(e) {
+    console.log("鼠标移出");
+    this.mask = "hide";
+    var title = this.titleContent;
+    if(title !== ""){
+      //var a = document.querySelector(".title");
+      console.log(this.shadowRoot.children[1].children[0].className);
+      this.shadowRoot.children[1].children[0].className += "ox-bubble-wangfanglan-box-ok";
+      //console.log(e.target.children);
+      //e.target.children.setAttribute('class','ox-bubble-wangfanglan-box-ok');
+    }else{
+      console.log(0)
+    }
+  }
+
+  ready() {
+    super.ready();
+    //hasTitle();
+    this.className = `ox-bubble-box ox-bubble-box-${this.type}`;
+>>>>>>> develop
   }
 }
 
---
+++ index.js
@@ -0,0 +1,29 @@
+/**
+ * @license
+ * Copyright (c) 2018 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
+
+import './node_modules/@webcomponents/webcomponentsjs/webcomponents-loader';
+import './elements/ox-button';
+ 
+ 
+
+
+
+
+
+// import './elements/ox-bubble-wangfanglan/ox-bubble-wangfanglan.js';
+import './elements/ox-bubble';
+import './elements/ox-bubble-main';
+import './elements/ox-bubble-alert';
+
+
+import './elements/ox-checkbox';
+import './elements/ox-modal-shaoxinheng';
+
+import './elements/ox-input-wangning';
